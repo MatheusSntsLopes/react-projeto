3dd87b40cea74ccffee375329b50005a143e11a2
matheus <abbadoncruel@hotmail.com>
2023-03-09 15:24:56 -0300

    unity test

---
+++ src/components/Button/Button.spec.jsx
@@ -0,0 +1,31 @@
+import { Button} from '.';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+describe('<Button text="load more" />', ()=> {
+    it('should render the button with the text "Load more"', ()=> {
+        render(<Button text="Load more" />);
+
+        const button = screen.getByRole('button', { name: /load more/i });
+        expect(button).toBeInTheDocument();
+    });
+
+    it('should call function on button click', ()=> {
+        const fn = jest.fn();
+        render(<Button text="Load more" onClick={fn} />);
+
+        const button = screen.getByRole('button', { name: /load more/i });
+
+       
+        userEvent.click(button);
+        expect(fn).toHaveBeenCalledTimes(1);
+    });
+
+    it('should be disable when disabled is true', ()=> {
+        
+        render(<Button text="Load more" disabled={true} />);
+
+        const button = screen.getByRole('button', { name: /load more/i });
+
+       expect(button).toBeDisabled();
+    });
+})
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,11 +1,82 @@
 
 import './styles.css';
-import { Component } from 'react';
+import { useState,useEffect, useCallback } from 'react';
 import { loadPosts } from '../../utils/load-posts';
 import { Posts } from '../../components/Posts';
 import { Button } from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
 
+//React com HOOKS
+export const HomeHook = () => {
+
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(3);
+  const [searchValue, setSearchValue] = useState('');
+
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+  const filtedPosts = !!searchValue ? posts.filter(post => post.title.toLowerCase().includes(searchValue.toLowerCase())) : posts;
+
+
+
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
+    const postsAndPhotos = await loadPosts();
+
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, [])
+
+  const loadMorePosts = () => {
+
+
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    setPosts(posts);
+    setPage(nextPage);
+
+  }
+
+  const handleSearch = (e) => {
+    const { value } = e.target;
+  
+    setSearchValue(value);
+  }
+
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+  
+  return (
+    <section className="container">
+
+      <div className="search-container">
+        {!!searchValue && (
+          <h1> Search value: {searchValue} </h1>
+        )}
+        <TextInput searchValue={searchValue} handleSearch={handleSearch} />
+      </div>
+
+      {filtedPosts.length > 0 && (
+        <Posts posts={filtedPosts} />
+      )}
+      {filtedPosts.length === 0 && (
+        <p>Não existe posts</p>
+      )}
+      <div className='button-container'>
+        {!searchValue && (
+          <Button text="Load" onClick={loadMorePosts} disabled={noMorePosts} />
+        )}
+
+      </div>
+
+    </section>
+  );
+}
+
+/* REACT BASICO
 export class Home extends Component {
 
   state = { posts: [], allPosts: [], page: 0, postsPerPage: 3, searchValue: '' };
@@ -42,7 +113,7 @@ export class Home extends Component {
     const filtedPosts = !!searchValue ? posts.filter(post => post.title.toLowerCase().includes(searchValue.toLowerCase())) : posts;
     return (
       <section className="container">
-        
+
         <div className="search-container">
           {!!searchValue && (
             <h1> Search value: {searchValue} </h1>
@@ -68,3 +139,4 @@ export class Home extends Component {
   }
 }
 
+*/
